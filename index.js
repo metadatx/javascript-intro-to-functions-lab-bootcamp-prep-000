c85bfb503046ab6095d8ea407b5d233f102e75e1
FIRAT AKSU <aksu_fr@yahoo.es>
2018-03-16 16:20:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   else if(string === "HELLO")
     return "YES INDEED!"
-  
+  else if(string === "I love you, Grandma.")
+    return ""
 }
 
 function logWhisper(string) {
